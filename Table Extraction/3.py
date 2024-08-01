dd3bdc334a0f10ef68f45dd2cbe23fb3598e045a
Abhigyan Sen <abhigyan.sen@datacoresystems.co.in>
2024-08-01 13:47:38 +0530

    Added Table Extraction

--- Table Extraction/3.py
+++ Table Extraction/3.py
@@ -3,7 +3,7 @@
 import tabula
 
 # URL
-# pdf_path = "https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqa0JtQW1MX2VMSi1KR1FoeWdEQUN3VURWLXdDUXxBQ3Jtc0trUFlILTZIT1pWZVhjSjdKM2JYdUJid3NSVmZISTJsT0h5TmpXNTZDRjVFSEpPUXdPYllYQVhySWptMU02YS1UZkJnVHl0RFl3bHgtcU5GQmoyODVLN1BoTHdTbFEtTm5pMXZ0ZUNpZVFIYzQzdy1iSQ&q=https%3A%2F%2Fsedl.org%2Fafterschool%2Ftoolkits%2Fscience%2Fpdf%2Fast_sci_data_tables_sample.pdf&v=tEFAFQXaOWw"
+# pdf_path = "https://sedl.org/afterschool/toolkits/science/pdf/ast_sci_data_tables_sample.pdf"
 pdf_path = "demoPDF.pdf"
 
 dfs = tabula.read_pdf(pdf_path, pages='all')
